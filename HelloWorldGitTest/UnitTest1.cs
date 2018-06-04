997363c522704572811db29a056629578f33ded8
Hiren Bhavsar <hbhavsar01@hotmail.com>
2018-06-04 16:31:09 -0400

    first commit

---
+++ HelloWorldGitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string text = "Hello World!";
+
+            Assert.AreEqual("Hello World!", text);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string text = "Hello BAE!";
+
+            Assert.AreEqual("Hello BAE!", text);
+        }
+    }
+}
